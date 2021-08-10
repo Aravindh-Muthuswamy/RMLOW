48b088eed7f75215020684b0f4024e0bc2c1692c
Aravindh <31213085+Aravindh-Muthuswamy@users.noreply.github.com>
2021-08-10 17:55:54 +0930

    test script update

---
+++ invoke.py
@@ -0,0 +1,12 @@
+import os
+import sys
+if __name__ == "__main__":
+    print(f"Arguments count: {len(sys.argv)}")
+    args = 1
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i:>6}: {arg}")
+    print("asdasdfasdf : " + str(sys.argv[1]))
+    if sys.argv[2] == "1":
+        os.system('python linear.py')
+    elif sys.argv[2] == "2":
+        os.system('python logistic.py')
